e8fcb7be8c3339e35742ba25b90152613b216944
Pierre De Wulf <pierredewulf31@gmail.com>
2014-10-08 00:11:41 +0200

    FIX maj mama, donnER, interligne push 2

--- html/menu.php
+++ html/menu.php
@@ -4,7 +4,7 @@
         <div class="col-lg-12 menu-text-content">
             <div class="col-lg-2 menu-content">
                 <class class="col-lg-12 menu-href menu-href-ac ">
-                    <a href="index.php"> Acceuil</a>
+                    <a href="index.php"> Accueil</a>
                 </class>
                 <class class="col-lg-12 menu-href menu-href-1 ">
                     <a href="decouvrir.php?sheet=quiSommesNous"
@@ -20,7 +20,7 @@
                             if ($nav == 1) { echo 'class="current"'; }
                             else {echo 'class=""';}
                         ?>
-                    > Cadicea mama</a>
+                    > Cadicea Mama</a>
                 </class>
                 <class class="col-lg-12 menu-href">
                     <a href="decouvrir.php?sheet=gamme"
