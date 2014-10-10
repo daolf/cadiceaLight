f011800fe8038f06771decf778ff63d78812cf88
Pierre De Wulf <pierredewulf31@gmail.com>
2014-10-11 01:53:16 +0200

    ADD slider version mobile pour la gamme

--- app/app.js
+++ app/app.js
@@ -1,5 +1,52 @@
 
 var main = function(){
+	
+	// Ici gestion du slide pour résolution téléphone portable
+	if (window.matchMedia('(max-width: 991px)').matches)
+	{
+
+		var currentProd = $('.activeProduct');
+		$('.productContain').css('display','none');
+		currentProd.fadeIn(600);
+
+	 $('.arrow-next').click(function(){
+    var currentProd = $('.activeProduct');
+    var currentDot = $('.active-dot');
+    var nextProd = currentProd.next();
+    var nextDot = currentDot.next();
+    if(nextProd.length === 0){
+      nextProd = $('.productContain').first();
+      nextDot= $('.dot').first();
+    }
+
+    currentProd.fadeOut(600);
+    currentProd.removeClass('activeProduct');
+    currentDot.removeClass('active-dot');
+
+    nextProd.fadeIn(600);
+    nextProd.addClass('activeProduct');
+    nextDot.addClass('active-dot');
+    
+  	});
+  
+   $('.arrow-prev').click(function(){
+    var currentProd = $('.activeProduct');
+    var currentDot = $('.active-dot');
+    var nextProd = currentProd.prev();
+    var nextDot = currentDot.prev();
+    if(nextProd.length === 0){
+      nextProd = $('.productContain').last();
+      nextDot= $('.dot').last();
+    }
+    currentProd.fadeOut(600);
+    currentProd.removeClass('activeProduct');
+    currentDot.removeClass('active-dot');
+    nextProd.fadeIn(600);
+    nextProd.addClass('activeProduct');
+    nextDot.addClass('active-dot'); 
+ 	 });
+	
+	}	
 
 
 
@@ -35,11 +82,6 @@ var main = function(){
 		}
 
 	})
-
-
-
-
-
 
 
 }
