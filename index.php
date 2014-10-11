db5d4d010d929c86fd785f84dfaed535400626b2
ksahin <sahin.kevin@gmail.com>
2014-10-11 16:22:50 +0200

    ajout balises meta

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
   	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 	<![endif]-->
+	<meta name="description" content="Lait Infantile Français" />
+	<meta name="keywords" content="Cadicea, Lait infantile, md-fonscare" />
 	<title>Cadicea</title>
 </head>
 <body>
